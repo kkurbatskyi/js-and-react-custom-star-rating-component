e22e907a01e289da66112a8321a1d282315bd945
kkurbatskyi <57308179+kkurbatskyi@users.noreply.github.com>
2022-03-16 16:20:28 -0700

    Removed the files to replace them with suitable sub-repos

--- Q2JS/q2-minimum-react/scripts/index.js
+++
@@ -1,6 +0,0 @@
-import renderProducts from "./products.js"
-import renderRatings from "./ratings.js"
-
-document.getElementById("maxStars").onchange = () => {renderProducts(); renderRatings();}
-renderProducts();
-renderRatings();
